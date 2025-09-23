af4c8e92401395790b874d71ad3bd321c4c0cc9a
Collins Opiayo <opiayocollins2@gmail.com>
2025-09-23 20:23:19 +0300

    feat: Fix frontend-backend communication and add signup functionality

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch('/check_session').then((r) => {
+    fetch('http://127.0.0.1:5555/signup').then((r) => {
       if (r.ok) {
         r.json().then(setUser);
       }
