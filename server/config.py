3719869b860ca3ae450818a37f579ca5b83cf99c
Collins Opiayo <opiayocollins2@gmail.com>
2025-09-23 14:02:59 +0300

    added config.py

--- server/config.py
+++ server/config.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'a_very_secret_key'  # Use an environment variable in production
+app.config['SECRET_KEY'] = 'a_very_secret_key'  # Use an environment variable in production.
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
